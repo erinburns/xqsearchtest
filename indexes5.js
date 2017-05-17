7896a8231afe33ca71e6e1e43ec4bf1eb3640632
erinb52s <erinb52s@gmail.com>
2017-05-17 23:14:45 +0100

    login signin buttons not appearing

--- indexes5.js
+++ indexes5.js
@@ -60,18 +60,25 @@
         firebase.auth().signOut();
     });
 
-    // realtime authentication listener
+    // realtime authentication listener. no console errors but buttons not appearing
     firebase.auth().onAuthStateChanged(function (firebaseUser) {
         if (firebaseUser) {
             console.log(firebaseUser);
             btnLogout.classList.remove('hide');
             btnSignUp.classList.add('hide');
             btnLogin.classList.add('hide');
+            }
+        if (firebaseUser) {
+          firebaseUser.sendEmailVerification().then(function() {
+            // Email sent.
+          }, function(error) {
+            // An error happened.
+          });
         } else {
-            console.log('not logged in');
+	       console.log('not logged in');
             btnLogout.classList.add('hide');
             btnSignUp.classList.remove('hide');
-            btnLogin.classList.remove('hide');
-        } // end if statement
-    }); // end function
-})();
+            btnLogin.classList.remove('hide');        
+	   } // else
+    })(); 
+})(); // end Js wrapper
