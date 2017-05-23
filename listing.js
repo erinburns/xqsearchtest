ea8ff5dc5496d6ff9e9cbca3b9ecf0fc7a784569
erinb52s <erinb52s@gmail.com>
2017-05-23 22:43:03 +0100

    add browse listings page and display listings js

--- listing.js
+++ listing.js
@@ -17,31 +17,52 @@
 var events = firebase.database().ref("events");
     
     // Save a new event to the database, using the input in the form
+    // TODO: find out how to link to the user's profile - might need to bind to a hidden form field
 var submitEvent = function () {
     // Get input values from each of the form elements
     var title = $("#eventtitle").val();
     var description = $("#desc").val();
     var begTime = $("#starttime").val();
     var finTime = $("#endtime").val();
+    var place = $("#city").val();
     var cost = $("#price").val();
     
     // Push a new event to the database using those values
-  events.push({
+  events.push({  // items on left are db labels right are variable names
         "title": title,
         "description": description,
         "starttime": begTime,
         "endtime": finTime,
+        "city": place, // TODO add city province field to form
         "price": cost
         });
     };  // end submitEvent function
+    
+    
+    // displays event listings on browse.html
+    events.limitToLast(1).on('child_added', function(childSnapshot) {
+        
+        // retrieves data from db
+        event = childSnapshot.val(); // note there is no 's' at end of this event
+        
+        // Display the event data in HTML
+        $("#browsetitle").html(event.title)
+        $("#browsecity").html(event.city)
+        $("#browsedate").html(event.starttime)
+        $("#browseprice").html(event.price)
+        
+        // TODO: find out how to link to the listing details from db ID
+        // Make the link actually work and direct to the URL provided
+        // $("#link").attr("href", event.link) 
+    });
 
     
-$(window).load(function () {
+    $(window).load(function () {
 
-  // Find the HTML element with the id recommendationForm, and when the submit
-  // event is triggered on that element, call submitRecommendation.
-  $("#listingform").submit(submitEvent);
+      // Find the HTML element with the id recommendationForm, and when the submit
+      // event is triggered on that element, call submitRecommendation.
+      $("#listingform").submit(submitEvent);
 
-}); // end window load
+    }); // end window load
     
 })(); // end file wrapper
